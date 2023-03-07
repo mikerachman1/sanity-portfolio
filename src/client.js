811bb2f4c02b4dba8f0af411b3c1e5b8f1e29f85
Mike Rachman <mikerachman1@gmail.com>
2023-03-07 15:33:47 -0500

    Created Post component

--- src/client.js
+++ src/client.js
@@ -1,6 +1,15 @@
-import { SanityClient } from "@sanity/client";
+// import { SanityClient } from "@sanity/client";
 
-export default SanityClient({
+// export default new SanityClient({
+//   projectId: "ewu9y6v1",
+//   dataset: "production"
+// })
+
+import { createClient } from "@sanity/client";
+
+export const client = createClient({
   projectId: "ewu9y6v1",
-  dataset: "production"
+  dataset: "production",
+  apiVersion: '2023-03-07',
+  useCdn: true
 })
