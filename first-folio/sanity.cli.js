48ca2e43b1b73d53e94210a39b3837bf5177c4d8
Mike Rachman <mikerachman1@gmail.com>
2023-03-07 11:42:24 -0500

    connect sanity and react app

---
+++ first-folio/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'ewu9y6v1',
+    dataset: 'production'
+  }
+})
---
+++ src/client.js
@@ -0,0 +1,6 @@
+import { SanityClient } from "@sanity/client";
+
+export default SanityClient({
+  projectId: "ewu9y6v1",
+  dataset: "production"
+})
