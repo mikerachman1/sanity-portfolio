1f265edb9508ae0f7f711c8347ae3b5dd44e9027
Mike Rachman <mikerachman1@gmail.com>
2023-03-08 15:49:51 -0500

    About component created

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,48 @@
+import { useEffect, useState } from "react";
+import { client } from "../client";
+import imageUrlBuilder from '@sanity/image-url';
+import view from "../view.jpg";
+import BlockContent from "@sanity/block-content-to-react";
+
+
+
+const builder = imageUrlBuilder(client);
+function urlFor(source) {
+  return builder.image(source)
+}
 
 const About = () => {
+  const [author, setAuthor] = useState(null);
+
+  useEffect(() => {
+    client.fetch(`*[_type == "author"]{
+      name,
+      bio,
+      "authorImage": image.asset->url
+    }`).then((data) => setAuthor(data[0]))
+    .catch(console.error)
+  }, []);
+
+  if(!author) return <div>Loading...</div>
+
   return (
-    <h1>About</h1>
+    <main className="relative">
+      <img src={view} alt="nice view" className="absolute w-full" />
+      <div className="p-10 lg:pt-48 container mx-auto relative">
+        <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
+          <img src={urlFor(author.authorImage).url()} alt={author.name}  className="rounded w-32 h-64 lg:w-64 lg:h-64 mr-8"/>
+          <div className="text-lg flex-col justify-center">
+            <h1 className="cursive text-6xl text-green-300 mb-4">
+              Hey there, I'm{" "}
+              <span className="text-green-100">{author.name}</span>
+            </h1>
+            <div className="prose lg:prose-xl text-white">
+              <BlockContent blocks={author.bio} projectId="ewu9y6v1" dataset="production"/>
+            </div>
+          </div>
+        </section>
+      </div>
+    </main>
   );
 };
 
